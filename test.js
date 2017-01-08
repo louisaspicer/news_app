68dc77670c5a3a8c213823af3ec530e7476f7ccc
Louisa <louisaspicer@gmail.com>
2017-01-08 00:59:33 +0000

    Spiking code - have been able to get the headlines' id for later use

--- test.js
+++ test.js
@@ -1,23 +1,40 @@
+
+
+// myRequest.onreadystatechange = function() {
+//   // console.log(myRequest.responseText)
+//   json = myRequest.responseText;
+//   // console.log(json)
+//   obj = JSON.parse(json);
+//   console.log(obj);
+//
+//   if (myRequest.readyState === 4) {
+//     var p = document.createElement("p");
+//     // var t = document.createTextNode(obj.response.content.fields.bodyText);
+//     p.appendChild(t);
+//     document.getElementById("app").appendChild(p);
+//   }
+// };
+
+
+// var url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/football/2017/jan/07/preston-north-end-arsenal-fa-cup-match-report?show-fields=body'
+// var url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/lifeandstyle/2017/jan/08/botox-nation-america-addiction-crack-like-cosmetic-procedures?show-fields=all';
+
+// this generates a request for the headlines' id, so that they can be used to access each individual articles' data later.
 var myRequest = new XMLHttpRequest();
 
 myRequest.onreadystatechange = function() {
-  // console.log(myRequest.responseText)
-  json = myRequest.responseText;
-  // console.log(json)
-  obj = JSON.parse(json);
-  console.log(obj);
-  // console.log(obj.response.content.fields.body)
+  var json = myRequest.responseText;
+  var obj = JSON.parse(json);
+  var headlines = [];
   if (myRequest.readyState === 4) {
-    var p = document.createElement("p");
-    var t = document.createTextNode(myRequest.responseText);
+    var length = obj.response.results.length;
+    for (var index = 0; index < length; index++) {
+      headlines.push(obj.response.results[index].id);
+    }
+    console.log(headlines);
   }
+
 };
-
-// var url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body';
-// var url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/sport/2017/jan/07/saracens-exeter-premiership-match-report';
-
-// var url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/football/2017/jan/07/preston-north-end-arsenal-fa-cup-match-report?show-fields=body'
-
 var headlines = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest&q=headline?show-fields=body';
 myRequest.open('GET', headlines, true);
 myRequest.send(null);
